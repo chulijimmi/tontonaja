7d22346fcdbe6ec38ae5521f6219f1635b6c213b
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2020-03-27 15:15:42 +0700

    Updates

---
+++ precache-manifest.3f6ee91b7c7110953cd1628a723e8351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81b745e05ef1d5c0dc359f7cd73d088",
+    "url": "/tontonaja/index.html"
+  },
+  {
+    "revision": "a09b589e82d4f6549b4b",
+    "url": "/tontonaja/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5cf5e3087d3a97375001",
+    "url": "/tontonaja/static/js/2.6b57ddc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tontonaja/static/js/2.6b57ddc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09b589e82d4f6549b4b",
+    "url": "/tontonaja/static/js/main.9310565e.chunk.js"
+  },
+  {
+    "revision": "55a9d741a471aac1626e",
+    "url": "/tontonaja/static/js/runtime-main.5e12bc16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tontonaja/static/media/logo.5d5d9eef.svg"
+  }
+]);
